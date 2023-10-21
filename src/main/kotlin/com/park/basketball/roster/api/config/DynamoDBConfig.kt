4a468cee9d6656aca1c233c4a281d3d49301b859
bullkan94 <bullkan94@gmail.com>
2023-10-21 15:09:21 +0900

    Change db client config

--- src/main/kotlin/com/park/basketball/roster/api/config/DynamoDBConfig.kt
+++ src/main/kotlin/com/park/basketball/roster/api/config/DynamoDBConfig.kt
@@ -2,44 +2,20 @@ package com.park.basketball.roster.api.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 
 @Configuration
 class DynamoDBConfig {
-    @Bean(value = ["rosterDbClient"])
-    @Profile(value = ["local"])
-    fun localRosterDbClient(): DynamoDbEnhancedClient {
+    @Bean
+    fun rosterDbClient(): DynamoDbEnhancedClient {
         return DynamoDbEnhancedClient.builder()
             .dynamoDbClient(
                 DynamoDbClient.builder()
-                    .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                            AwsBasicCredentials.create(
-                                "",
-                                "",
-                            ),
-                        ),
-                    )
-                    .region(Region.AP_NORTHEAST_1)
-                    .build(),
-            )
-            .build()
-    }
-
-    @Bean(value = ["rosterDbClient"])
-    @Profile(value = ["dev", "prod"])
-    fun remoteRosterDbClient(): DynamoDbEnhancedClient {
-        return DynamoDbEnhancedClient.builder()
-            .dynamoDbClient(
-                DynamoDbClient.builder()
-                    .credentialsProvider(ProfileCredentialsProvider.create())
-                    .region(Region.AP_NORTHEAST_1)
+                    .credentialsProvider(ProfileCredentialsProvider.create("014104314849_AdministratorAccess"))
+                    .region(Region.AP_NORTHEAST_2)
                     .build(),
             )
             .build()
