721f73c179f85826d0c3060662b42b383fb7187e
bullkan94 <bullkan94@gmail.com>
2023-10-31 13:14:44 +0900

    Change credential provider class

--- src/main/kotlin/com/park/basketball/roster/api/config/DynamoDBConfig.kt
+++ src/main/kotlin/com/park/basketball/roster/api/config/DynamoDBConfig.kt
@@ -2,7 +2,7 @@ package com.park.basketball.roster.api.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
@@ -14,7 +14,7 @@ class DynamoDBConfig {
         return DynamoDbEnhancedClient.builder()
             .dynamoDbClient(
                 DynamoDbClient.builder()
-                    .credentialsProvider(ProfileCredentialsProvider.create("014104314849_AdministratorAccess"))
+                    .credentialsProvider(DefaultCredentialsProvider.create())
                     .region(Region.AP_NORTHEAST_2)
                     .build(),
             )
