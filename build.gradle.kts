5357e59999fb31af4c6df346e97cde26cfb8d574
bullkan94 <bullkan94@gmail.com>
2023-10-31 10:43:43 +0900

    Add actuator dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     // spring
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
 
     // mapstruct
